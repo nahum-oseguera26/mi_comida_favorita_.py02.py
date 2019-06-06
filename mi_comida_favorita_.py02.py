60e2245a13c50ed05488464fdc6e8921a9c9787b
nahum-oseguera26 <49953527+nahum-oseguera26@users.noreply.github.com>
2019-06-05 22:43:05 -0600

    Add files via upload

---
+++ mi_comida_favorita_.py02.py
@@ -0,0 +1,40 @@
+comidas = [ " Sopa de pollo = Sopa de pollo " .
+    " Arroz y frijoles = Arroz y frijoles " .
+    " Sopa de Mondongo = Sopa de mondongo " .
+    " Sopa De Carne De Res = Sopa De Res " .
+    " Mariscos = Sopa marinera " .
+    " Espagueti = Espagueti " .
+    " Pollo frito = Pollo frito " .
+	" Pollo Asado = Pollo Asado "
+	" Baleadas "
+	" Tortillas Con Quesillo "
+	" sopa de caracol "
+	" Pescado "
+	" Emparedado "
+	" Chorizo ​​a la parilla "
+	" Chuleta en salsa de hongo "
+	" Papas fritas " 
+	" Salmón En Salsa De Hongo "
+        " sopa marinera "
+	" Pollo chuco "
+	" sopa de frijoles " ]
+
+def imprimir menu ()
+	imprimir ( " escoge tus 5 comidas favoritas: " )
+	para comida en comidas
+		de impresión (comida)
+		
+imprimir_menu ()		
+
+imprimir ()
+comidas favoritas = [ " Arroz y frijoles = Arroz y frijoles " .
+    " Mariscos = Sopa marinera " .
+    " Pescado " , " Emparedado "
+	" Tortillas Con Quesillo " ]
+	
+	def imprimir menu ()
+	Imprimir ( " estan son mis 5 comidas favoritas: " )
+	para comida en comidas
+		de impresión (comida)
+		
+imprimir_menu ()
